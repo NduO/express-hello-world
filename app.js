27e76eb4b9b2e239504bb56c1430be32a7624149
NduO <38164646+NduO@users.noreply.github.com>
2020-09-20 21:24:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.send("Hello from Render via futopay!"));
+app.get("/", (req, res) => {
+  res.send("Hello from Render via futopay!");
+});
+
+app.get("/accounts", (req, res) => {
+  res.send(req.body);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
